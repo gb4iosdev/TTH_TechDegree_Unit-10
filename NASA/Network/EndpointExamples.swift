7902b0bf033dd6e912e3be7a2ab56d8c0da86cea
Gavin Butler <gavinrossbutler@gmail.com>
2019-12-25 15:40:29 -0500

    Initial Commit - started draft UI

---
+++ NASA/Network/EndpointExamples.swift
@@ -0,0 +1,24 @@
+//
+//  EndpointExamples.swift
+//  NASA
+//
+//  Created by Gavin Butler on 25-12-2019.
+//  Copyright © 2019 Gavin Butler. All rights reserved.
+//
+
+//
+//  API Key:
+//  4Ye02d6tWRNIZvP5a9RMOnFbePacsNy6ZfwIcW9l
+
+//  Account Email:
+//  gavin.butler01@bigpond.com
+
+//  Account ID:
+//  b2f38bac-e24b-4214-b7e0-42399dde7d6e
+
+//  Example:
+//  https://api.nasa.gov/planetary/apod?api_key=4Ye02d6tWRNIZvP5a9RMOnFbePacsNy6ZfwIcW9l
+
+
+
+
